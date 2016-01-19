76d1e4959c080aa36dc3739091bc44aace3d7c53
Jack Cook <hello@jackcook.nyc>
2016-01-18 21:53:53 -0500

    Fixed .gitignore

--- Example/Pods/Target Support Files/BeamAPI/BeamAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BeamAPI : NSObject
-@end
-@implementation PodsDummy_BeamAPI
-@end
--- Example/Pods/Target Support Files/BeamAPI/BeamAPI-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double BeamAPIVersionNumber;
-FOUNDATION_EXPORT const unsigned char BeamAPIVersionString[];
-
