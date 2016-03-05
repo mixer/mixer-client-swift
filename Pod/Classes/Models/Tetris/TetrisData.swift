bf9511b1a6144008852025b8af050e375529f0ee
Jack Cook <hello@jackcook.nyc>
2016-03-05 15:13:37 -0500

    Added more TetrisData keys

--- Pod/Classes/Models/Tetris/TetrisData.swift
+++ Pod/Classes/Models/Tetris/TetrisData.swift
@@ -11,10 +11,16 @@ import SwiftyJSON
 public struct TetrisData {
     
     public let address: String?
+    public let key: String?
+    public let userId: Int?
+    public let influence: Int?
     public let version: TetrisVersion?
     
     public init(json: JSON) {
         address = json["address"].string
+        key = json["key"].string
+        userId = json["user"].int
+        influence = json["influence"].int
         version = json["version"].dictionary == nil ? nil : TetrisVersion(json: json["version"])
     }
 }
