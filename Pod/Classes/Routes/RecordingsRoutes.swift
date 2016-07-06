1767552fa2448bb1bed72a5acdb1abe6fcecb5a3
Jack Cook <hello@jackcook.nyc>
2016-07-06 17:57:33 -0400

    Documented recordings endpoints

--- Pod/Classes/Routes/RecordingsRoutes.swift
+++ Pod/Classes/Routes/RecordingsRoutes.swift
@@ -6,8 +6,31 @@
 //
 //
 
+/// Routes that can be used to interact with and retrieve recording data.
 public class RecordingsRoutes {
     
+    // MARK: Acting on Recordings
+    
+    /**
+     Marks a recording as seen for the authenticated user.
+     
+     :param: id The id of the recording being marked.
+     :param: completion An optional completion block that fires when the action has been completed.
+     */
+    public func markRecordingSeen(id: Int, completion: ((error: BeamRequestError?) -> Void)?) {
+        BeamRequest.request("/recordings/\(id)/seen", requestType: "POST") { (json, error) in
+            completion?(error: error)
+        }
+    }
+    
+    // MARK: Retrieving Recordings
+    
+    /**
+     Retrieves a recording with the specified identifier.
+     
+     :param: id The identifier of the recording being retrieved.
+     :param: completion An optional completion block with retrieved recording data.
+     */
     public func getRecording(id: Int, completion: ((recording: BeamRecording?, error: BeamRequestError?) -> Void)?) {
         BeamRequest.request("/recordings/\(id)") { (json, error) in
             guard let json = json else {
@@ -20,24 +43,12 @@ public class RecordingsRoutes {
         }
     }
     
-    public func getRecordings(completion: ((recordings: [BeamRecording]?, error: BeamRequestError?) -> Void)?) {
-        BeamRequest.request("/recordings") { (json, error) in
-            guard let json = json, let recordings = json.array else {
-                completion?(recordings: nil, error: error)
-                return
-            }
-            
-            var retrievedRecordings = [BeamRecording]()
-            
-            for recording in recordings {
-                let retrievedRecording = BeamRecording(json: recording)
-                retrievedRecordings.append(retrievedRecording)
-            }
-            
-            completion?(recordings: retrievedRecordings, error: error)
-        }
-    }
-    
+    /**
+     Retrieves all recordings on a specified channel.
+     
+     :param: channelId The id of the channel recordings are being retrieved from.
+     :param: completion An optional completion block with the retrieved recordings' data.
+     */
     public func getRecordingsByChannel(channelId: Int, completion: ((recordings: [BeamRecording]?, error: BeamRequestError?) -> Void)?) {
         BeamRequest.request("/recordings?where=channelId.eq.\(channelId)") { (json, error) in
             guard let json = json, let recordings = json.array else {
@@ -53,12 +64,6 @@ public class RecordingsRoutes {
             }
             
             completion?(recordings: retrievedRecordings, error: error)
-        }
-    }
-    
-    public func markRecordingSeen(id: Int, completion: ((error: BeamRequestError?) -> Void)?) {
-        BeamRequest.request("/recordings/\(id)/seen", requestType: "POST") { (json, error) in
-            completion?(error: error)
         }
     }
 }
