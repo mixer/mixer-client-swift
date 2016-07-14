c898c9028d5dcb0d2c77b8f094828674f7c7c9eb
Jack Cook <hello@jackcook.nyc>
2016-07-14 01:17:55 -0400

    Fixed an issue with old emoticon spritesheets

--- Pod/Classes/Models/BeamEmoticon.swift
+++ Pod/Classes/Models/BeamEmoticon.swift
@@ -17,14 +17,20 @@ public struct BeamEmoticon {
     /// The emoticon's position in its spritesheet.
     public let position: CGPoint
     
+    /// The emoticon's size in its spritesheet.
+    public let size: CGSize
+    
     /// Used to initialize an emoticon given JSON data.
     init(name: String, json: JSON) {
         self.name = name
         
         let x = json["x"].int ?? 0
         let y = json["y"].int ?? 0
+        let w = json["width"].int ?? 22
+        let h = json["height"].int ?? 22
         
         position = CGPointMake(CGFloat(x), CGFloat(y))
+        size = CGSizeMake(CGFloat(w), CGFloat(h))
     }
     
     /**
@@ -34,7 +40,7 @@ public struct BeamEmoticon {
      :returns: The cropped emoticon image.
      */
     public func imageFromSpritesheet(spritesheet: UIImage) -> UIImage? {
-        let croppedRect = CGRectMake(position.x, position.y, 22, 22)
+        let croppedRect = CGRectMake(position.x, position.y, size.width, size.height)
         if let image = CGImageCreateWithImageInRect(spritesheet.CGImage, croppedRect) {
             return UIImage(CGImage: image)
         }
