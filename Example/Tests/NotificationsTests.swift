b5af87673825026150b6a1c39fe508321ab096ec
Jack Cook <hello@jackcook.nyc>
2016-08-09 14:57:36 -0400

    Added a route for deleting notification transports

--- Example/Tests/NotificationsTests.swift
+++ Example/Tests/NotificationsTests.swift
@@ -12,7 +12,42 @@ import XCTest
 class NotificationsTests: XCTestCase {
     
     let transport = "push"
+    let transportId = 840858
     let userId = 278
+    
+    func testsMarkNotificationsAsRead() {
+        let expectation = expectationWithDescription("tests marking notifications as read")
+        
+        BeamClient.sharedClient.notifications.markNotificationsAsRead(userId, beforeDate: NSDate()) { (error) in
+            XCTAssert(error == .InvalidCredentials)
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+    
+    func testsUpdateTransport() {
+        let expectation = expectationWithDescription("tests the updating a notification transport endpoint")
+        
+        BeamClient.sharedClient.notifications.updateNotificationTransport(userId, transport: transport, data: nil, settings: nil) { (transport, error) in
+            XCTAssertNil(transport)
+            XCTAssert(error == .InvalidCredentials)
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+    
+    func testsDeleteTransport() {
+        let expectation = expectationWithDescription("tests the delete transport endpoint")
+        
+        BeamClient.sharedClient.notifications.deleteNotificationTransport(userId, transportId: transportId) { (error) in
+            XCTAssert(error == .InvalidCredentials)
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
     
     func testsNotifications() {
         let expectation = expectationWithDescription("tests the notifications endpoint")
@@ -31,18 +66,6 @@ class NotificationsTests: XCTestCase {
         
         BeamClient.sharedClient.notifications.getNotificationTransports(userId) { (transports, error) in
             XCTAssertNil(transports)
-            XCTAssert(error == .InvalidCredentials)
-            expectation.fulfill()
-        }
-        
-        waitForExpectationsWithTimeout(10, handler: nil)
-    }
-    
-    func testsUpdateTransport() {
-        let expectation = expectationWithDescription("tests the updating a notification transport endpoint")
-        
-        BeamClient.sharedClient.notifications.updateNotificationTransport(userId, transport: transport, data: nil, settings: nil) { (transport, error) in
-            XCTAssertNil(transport)
             XCTAssert(error == .InvalidCredentials)
             expectation.fulfill()
         }
--- Pod/Classes/Routes/NotificationsRoutes.swift
+++ Pod/Classes/Routes/NotificationsRoutes.swift
@@ -11,6 +11,75 @@ import SwiftyJSON
 /// Routes that can be used to interact with and retrieve notification data.
 public class NotificationsRoutes {
     
+    // MARK: Acting on Notifications
+    
+    /**
+     Marks all notifications sent before a given date as having been read.
+     
+     :param: userId The identifier of the user whose notifications are being marked as read.
+     :param: date The date before which notifications should be marked as read.
+     :param: completion An optional completion block with response data.
+     */
+    public func markNotificationsAsRead(userId: Int, beforeDate date: NSDate, completion: ((error: BeamRequestError?) -> Void)?) {
+        let formatter = NSDateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        let body = ["date": formatter.stringFromDate(date)]
+        
+        BeamRequest.request("/notifications/read", requestType: "POST", params: ["userId": "\(userId)"], body: body) { (json, error) in
+            completion?(error: error)
+        }
+    }
+    
+    /**
+     Creates a notification transport, or updates it if one exists with the same transport type.
+     
+     :param: userId The identifier of the user who will be using this notification transport.
+     :param: transport The type of transport being updated (e.g. email, push)
+     :param: data Transport-independent data about how it should work.
+     :param: settings Settings about when this transport should be used.
+     :param: turnAllOn If this is true and settings are not provided, all notifications for this transport will be turned on. This is false by default.
+     :param: completion An optional completion block with response data.
+     */
+    public func updateNotificationTransport(userId: Int, transport: String, data: [String: AnyObject]?, settings: [[String: AnyObject]]?, turnAllOn: Bool = false, completion: ((transport: BeamNotificationTransport?, error: BeamRequestError?) -> Void)?) {
+        let settingDict = settings == nil && turnAllOn ? ["*"] : []
+        
+        let body: [String: AnyObject] = [
+            "userId": "\(userId)",
+            "transport": transport,
+            "data": data ?? [:],
+            "settings": settings ?? [
+                [
+                    "type": "wentlive",
+                    "setting": settingDict
+                ]
+            ]
+        ]
+        
+        BeamRequest.request("/notifications/transports", requestType: "POST", body: body) { (json, error) in
+            guard let json = json where error == nil else {
+                completion?(transport: nil, error: error)
+                return
+            }
+            
+            let transport = BeamNotificationTransport(json: json)
+            completion?(transport: transport, error: error)
+        }
+    }
+    
+    /**
+     Deletes a notification transport.
+     
+     :param: userId The identifier of the user whose transport is being deleted.
+     :param: transportId The identifier of the transport being deleted.
+     :param: completion An optional completion block with response data.
+     */
+    public func deleteNotificationTransport(userId: Int, transportId: Int, completion: ((error: BeamRequestError?) -> Void)?) {
+        BeamRequest.request("/notifications/transports/\(transportId)", requestType: "DELETE", params: ["userId": "\(userId)"]) { (json, error) in
+            completion?(error: error)
+        }
+    }
+    
     // MARK: Retrieving Notifications
     
     /**
@@ -58,42 +127,6 @@ public class NotificationsRoutes {
             }
             
             completion?(transports: retrievedTransports, error: error)
-        }
-    }
-    
-    /**
-     Creates a notification transport, or updates it if one exists with the same transport type.
-     
-     :param: userId The identifier of the user who will be using this notification transport.
-     :param: transport The type of transport being updated (e.g. email, push)
-     :param: data Transport-independent data about how it should work.
-     :param: settings Settings about when this transport should be used.
-     :param: turnAllOn If this is true and settings are not provided, all notifications for this transport will be turned on. This is false by default.
-     :param: completion An optional completion block with response data.
-     */
-    public func updateNotificationTransport(userId: Int, transport: String, data: [String: AnyObject]?, settings: [[String: AnyObject]]?, turnAllOn: Bool = false, completion: ((transport: BeamNotificationTransport?, error: BeamRequestError?) -> Void)?) {
-        let settingDict = settings == nil && turnAllOn ? ["*"] : []
-        
-        let body: [String: AnyObject] = [
-            "userId": "\(userId)",
-            "transport": transport,
-            "data": data ?? [:],
-            "settings": settings ?? [
-                [
-                    "type": "wentlive",
-                    "setting": settingDict
-                ]
-            ]
-        ]
-        
-        BeamRequest.request("/notifications/transports", requestType: "POST", body: body) { (json, error) in
-            guard let json = json where error == nil else {
-                completion?(transport: nil, error: error)
-                return
-            }
-            
-            let transport = BeamNotificationTransport(json: json)
-            completion?(transport: transport, error: error)
         }
     }
 }
