767e830e25cd1543593c70906455425f2bb7511b
Jack Cook <hello@jackcook.nyc>
2016-08-19 07:35:38 -0400

    Updated VOD URL handling

--- Pod/Classes/Models/BeamVOD.swift
+++ Pod/Classes/Models/BeamVOD.swift
@@ -11,22 +11,40 @@ import SwiftyJSON
 public struct BeamVOD {
     
     public let id: Int
-    public let storageNode: String
-    public let mainUrl: String
+    public let storageNode: String?
+    public let baseUrl: String?
+    public let mainUrl: String?
     public let format: String
     public let createdAt: NSDate?
     public let updatedAt: NSDate?
     public let recordingId: Int
     
-    public var url: NSURL {
-        return NSURL(string: "https://\(storageNode)/\(id)/\(mainUrl)")!
+    public var url: NSURL? {
+        func filePath() -> String? {
+            return [
+                "dash": "manifest.mpd",
+                "hls": "manifest.m3u8",
+                "thumbnail": "source.json",
+                "raw": "source.mp4",
+                "chat": "source.json"
+            ][format]
+        }
+        
+        if let baseUrl = baseUrl, filePath = filePath() {
+            return NSURL(string: "\(baseUrl)\(filePath)")
+        } else if let storageNode = storageNode, mainUrl = mainUrl {
+            return NSURL(string: "https://\(storageNode)/\(id)/\(mainUrl)")
+        }
+        
+        return nil
     }
     
     init(json: JSON) {
         id = json["id"].int ?? 0
-        storageNode = json["storageNode"].string ?? ""
-        mainUrl = json["mainUrl"].string ?? ""
-        format = json["format"].string ?? ""
+        storageNode = json["storageNode"].string
+        baseUrl = json["baseUrl"].string
+        mainUrl = json["mainUrl"].string
+        format = json["format"].string ?? "unknown"
         createdAt = NSDate.fromBeam(json["createdAt"].string)
         updatedAt = NSDate.fromBeam(json["updatedAt"].string)
         recordingId = json["recordingId"].int ?? 0
