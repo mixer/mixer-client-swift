edd728b9d60acc9f082c91cacde7a0e5a5637f6f
Jack Cook <hello@jackcook.nyc>
2016-12-14 19:09:04 -0500

    Removed deprecated BeamVOD properties

--- Pod/Classes/Models/BeamVOD.swift
+++ Pod/Classes/Models/BeamVOD.swift
@@ -11,29 +11,23 @@ import SwiftyJSON
 public struct BeamVOD {
     
     public let id: Int
-    public let storageNode: String?
     public let baseUrl: String?
-    public let mainUrl: String?
     public let format: String
     public let createdAt: Date?
     public let updatedAt: Date?
     public let recordingId: Int
     
     public var url: URL? {
-        func filePath() -> String? {
-            return [
-                "dash": "manifest.mpd",
-                "hls": "manifest.m3u8",
-                "thumbnail": "source.json",
-                "raw": "source.mp4",
-                "chat": "source.json"
-            ][format]
-        }
+        let filePath = [
+            "dash": "manifest.mpd",
+            "hls": "manifest.m3u8",
+            "thumbnail": "source.json",
+            "raw": "source.mp4",
+            "chat": "source.json"
+        ][format]
         
-        if let baseUrl = baseUrl, let filePath = filePath() {
+        if let baseUrl = baseUrl, let filePath = filePath {
             return URL(string: "\(baseUrl)\(filePath)")
-        } else if let storageNode = storageNode, let mainUrl = mainUrl {
-            return URL(string: "https://\(storageNode)/\(id)/\(mainUrl)")
         }
         
         return nil
@@ -41,9 +35,7 @@ public struct BeamVOD {
     
     init(json: JSON) {
         id = json["id"].int ?? 0
-        storageNode = json["storageNode"].string
         baseUrl = json["baseUrl"].string
-        mainUrl = json["mainUrl"].string
         format = json["format"].string ?? "unknown"
         createdAt = Date.fromBeam(json["createdAt"].string)
         updatedAt = Date.fromBeam(json["updatedAt"].string)
