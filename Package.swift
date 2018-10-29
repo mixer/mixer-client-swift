d12e0d101abb63ea7cf3b09fa44f28f0bed7f8a0
Cihan Tas <cihantas@users.noreply.github.com>
2018-10-29 10:51:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     name: "MixerAPI",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "3.1.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
         .Package(url: "https://github.com/daltoniam/Starscream.git", from: "2.0.0")
     ]
 )
